372d436559f4af274b5ed32f4dc2a35d1c4c71e0
Suborna Rani Talukder <153237224+suborna12@users.noreply.github.com>
2026-01-08 03:00:41 -0800

    Update The_hash_lookup.py

--- Day8/The_hash_lookup.py
+++ Day8/The_hash_lookup.py
@@ -1,27 +1,13 @@
-class HashSet:
-    def __init__(self, total_buckets=10):
-        self.total_buckets = total_buckets
-        self.buckets = [None] * total_buckets
-
-    def add(self, value):
-        index = calculate_hash(value) % self.total_buckets
-        self.buckets[index] = value
-
-def calculate_hash(value):
-    return hash(value)
-
 def set_contains(set, target):
-    hash_value = calculate_hash(target)
-    bucket_index = hash_value % set.total_buckets
-    memory_slot = set.buckets[bucket_index]
-    if memory_slot == target:
-        return True
-    else:
-        return False
+    index = hash(target) % len(set)
+    return target in set[index]
 
-list_n = list(range(-1000, 1000))
-set_n = set(list_n)
+
+
+set = [[] for _ in range(2000)]
+
+for i in range(-1000, 1000):
+    index = hash(i) % len(set)
+    set[index].append(i)
+
 print(set_contains(set, -5))
-
-
-    
